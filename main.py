e8ea5a9f474fe2ac2bbc5f32cabde5214020f01b
Karol <rogo.karol@gmail.com>
2021-05-22 23:53:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+from fastapi import Cookie, FastAPI, HTTPException, Query, Request, Response
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+from typing import List
+
+
+app = FastAPI()
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = "message.api.practise@gmail.com",
+    MAIL_PASSWORD = "Message123.",
+    MAIL_FROM = "message.api.practise@gmail.com",
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_FROM_NAME= "MessageAPI",
+    MAIL_TLS = True,
+    MAIL_SSL = False,
+    USE_CREDENTIALS = True
+)
+
+
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect("messages.db")
+    app.db_connection.text_factory = lambda b: b.decode(errors="ignore")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
