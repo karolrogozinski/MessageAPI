618e41b551fc610bfcb5500982cee1f7ea9e1747
Karol <rogo.karol@gmail.com>
2021-05-23 02:29:27 +0200

    Implemented email sending

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def login(email: str):
     )
     fm = FastMail(conf)
     await fm.send_message(message)
-    return JSONResponse(status_code=200, content={"message": "email has been sent"})
+    return {"message": "email has been sent"})
 
 
 @app.get("/messages")
